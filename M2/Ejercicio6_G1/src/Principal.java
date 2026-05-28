132872bd07cff260fcebc60ffdd0b2c38e91c34f
Patricio <patricio.yanez@fundacionluterana.cl>
2026-05-28 08:50:46 -0400

    update

---
+++ M2/Ejercicio6_G1/src/Principal.java
@@ -0,0 +1,19 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int resultado = 2 + 4 * 5;
+		System.out.println("El resultado es: " + resultado);
+		int resultado2 = 2 + (4 * 5);
+		System.out.println("El resultado2 es: " + resultado2);
+		int resultado3 = 5 * 4 / 3;
+		System.out.println("El resultado3 es: " + resultado3);
+		double resultado4 = (double)5 * 4 / 3;
+		System.out.println("El resultado4 es: " + resultado4);
+		int resultado5 = 5 * (4 + 3);
+		System.out.println("El resultado5 es: " + resultado5);
+
+		
+	}
+
+}
