2a8fe4e41b7712d64c2f8fc101874d5b6799cb5b
Patricio <patricio.yanez@fundacionluterana.cl>
2026-06-11 20:04:36 -0400

    update

---
+++ M2/VeterinariaG1/src/Animal.java
@@ -0,0 +1,51 @@
+
+public class Animal {
+	// atributo 
+	protected String nombre;
+	protected double peso;
+	
+	// metodo constructor con y sin parametros
+
+	public Animal() {
+		this.nombre = "";
+		this.peso = 0.0;		
+	}
+	
+	public Animal(String nombre, double peso) {
+		this.nombre = nombre;
+		this.peso = peso;
+	}
+
+	// getter and setter accesor y mutador
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+/*		if(nombre.length() == 0)
+			System.out.println("Escriba nombre del animal");
+		else
+*/			
+			this.nombre = nombre;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	
+	// metodos personalizados (customer)
+	
+	public void imprimir() // mostrar los datos de los atributos de la clase
+	{
+		System.out.println("Nombre	: " + this.nombre);		
+		System.out.println("Peso	: " + this.peso);
+	}
+	
+	
+}
---
+++ M2/VeterinariaG1/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Perro perro1 = new Perro();
+		perro1.setNombre("firulais");
+		perro1.setPeso(10.0);
+		perro1.setRaza("Salchicha");
+		
+		perro1.imprimir();
+	}
+}
