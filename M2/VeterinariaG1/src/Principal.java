3215843d40dab958d9fff7a26922dc4fe72cba0d
Patricio <patricio.yanez@fundacionluterana.cl>
2026-06-16 21:01:55 -0400

    update

--- M2/VeterinariaG1/src/Principal.java
+++ M2/VeterinariaG1/src/Principal.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Principal {
 
@@ -33,6 +34,33 @@ public class Principal {
 
 		System.out.println("\n");
 		gato1.emitirSonido();
+		
+		System.out.println("*********************************************");
+//Creación y uso de colección ArrayList
+		// se debe importar
+		ArrayList<Animal> veterinaria = new ArrayList<Animal>();
+		
+		Perro p1 = new Perro("Thor1", 30.1, "San Bernardo");
+		Perro p2 = new Perro("Thor2", 30.1, "Mestizo");
+		Perro p3 = new Perro("Thor3", 30.1, "Galgo");
+		Gato g1 = new Gato("Michin1", 10, true);
+		Gato g2 = new Gato("cosmico", 10, true);
+		// guardar las mascotas
+		veterinaria.add(g1);
+		veterinaria.add(p1);
+		veterinaria.add(g2);
+		veterinaria.add(p2);
+		veterinaria.add(p3);
+
+		// recorrer
+		
+		for(Animal a: veterinaria)
+		{
+			//a.imprimir();
+			a.emitirSonido();
+			System.out.println("\n");			
+		}
+		
 		
 		
 	}
