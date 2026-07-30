259fbaef9ad90c0abc7cf48b2e122767ebc3646b
Patricio <pa.yaneza@outlook.cl>
2026-07-29 20:51:38 -0400

    update

--- M4/Proyecto01G1/app/src/main/java/com/example/proyecto01g1/MenuActivity.java
+++ M4/Proyecto01G1/app/src/main/java/com/example/proyecto01g1/MenuActivity.java
@@ -8,13 +8,44 @@ import android.view.MenuItem;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MenuActivity extends AppCompatActivity {
 
+    private FloatingActionButton fabPrincipal;
+    private FloatingActionButton fabCalculadora;
+    private FloatingActionButton fabOpcion2;
+    private FloatingActionButton fabOpcion3;
+    private boolean open = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
+// permite agregar y se despliegue el menu FAB
+        fabPrincipal = findViewById(R.id.fabPrincipal);
+        fabCalculadora = findViewById(R.id.fabCalculadora);
+        fabOpcion2 = findViewById(R.id.fabOpcion2);
+        fabOpcion3 = findViewById(R.id.fabOpcion3);
+
+        fabPrincipal.setOnClickListener(view -> {
+            if (open) {
+                fabCalculadora.show();
+                fabOpcion2.show();
+                fabOpcion3.show();
+            } else {
+                fabCalculadora.hide();
+                fabOpcion2.hide();
+                fabOpcion3.hide();
+            }
+            open = !open;
+        });
+        fabCalculadora.setOnClickListener(view -> {
+            Intent intent = new Intent(this, CalculadoraActivity.class);
+            startActivity(intent);
+        });
+
+
 
     }
     @Override
