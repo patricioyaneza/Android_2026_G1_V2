bb6edb0cd2e39453006fc39de0b5c5f0a5eec236
Patricio <pa.yaneza@outlook.cl>
2026-08-07 19:57:49 -0400

    update

--- M5/ProyectoKotlin/app/src/main/java/com/example/proyectokotlin/LoginActivity.kt
+++ M5/ProyectoKotlin/app/src/main/java/com/example/proyectokotlin/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.example.proyectokotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,20 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnAceptar = findViewById<ImageButton>(R.id.btnAceptar)
+        val txtEmail = findViewById<TextInputEditText>(R.id.txtEmail)
+
+
+        btnAceptar.setOnClickListener {
+//            Toast.makeText(this, "Boton Aceptar", Toast.LENGTH_SHORT).show()
+            var sp = getSharedPreferences("misDatos", MODE_PRIVATE)
+            sp.edit().putString("email", txtEmail.text.toString()).apply()
+
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
--- M5/ProyectoKotlin/app/src/main/java/com/example/proyectokotlin/MenuActivity.kt
+++ M5/ProyectoKotlin/app/src/main/java/com/example/proyectokotlin/MenuActivity.kt
@@ -1,6 +1,7 @@
 package com.example.proyectokotlin
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,12 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val sp = getSharedPreferences("misDatos", MODE_PRIVATE)
+
+        val txtEmailUsuario = findViewById<TextView>(R.id.txtEmailUsuario)
+        txtEmailUsuario.text = sp.getString("email", "")
+
+
     }
 }
