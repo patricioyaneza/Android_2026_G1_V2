6aa7cd5b75699b62e1010a1d12ead6648f68b7ae
Patricio <pa.yaneza@outlook.cl>
2026-08-18 21:30:24 -0400

    update

--- M5/ProyectoKotlinCicloVida/app/src/main/java/com/example/kotlinciclovidafragment/BlankFragment.kt
+++ M5/ProyectoKotlinCicloVida/app/src/main/java/com/example/kotlinciclovidafragment/BlankFragment.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinciclovidafragment
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +10,50 @@ import android.view.ViewGroup
 
 class BlankFragment : Fragment() {
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d("Fragment", "onAttach")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("Fragment", "onCreate")
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("Fragment", "onCreateView")
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("Fragment", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Fragment", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Fragment", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Fragment", "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("Fragment", "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Fragment", "onDestroy")
     }
 
 }
