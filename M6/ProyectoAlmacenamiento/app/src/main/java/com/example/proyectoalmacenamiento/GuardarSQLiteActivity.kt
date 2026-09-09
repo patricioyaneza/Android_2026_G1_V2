95547ccbec8dcecf82c9b2d43fbdc7e0b25a28fb
Patricio <pa.yaneza@outlook.cl>
2026-09-09 20:08:57 -0300

    update

--- M6/ProyectoAlmacenamiento/app/src/main/java/com/example/proyectoalmacenamiento/GuardarSQLiteActivity.kt
+++ M6/ProyectoAlmacenamiento/app/src/main/java/com/example/proyectoalmacenamiento/GuardarSQLiteActivity.kt
@@ -1,11 +1,15 @@
 package com.example.proyectoalmacenamiento
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectoalmacenamiento.data.AppDatabase
 
 
 import com.example.proyectoalmacenamiento.databinding.ActivityGuardarSqliteBinding
-
+import com.example.proyectoalmacenamiento.model.Usuario
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class GuardarSQLiteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGuardarSqliteBinding
@@ -14,5 +18,34 @@ class GuardarSQLiteActivity : AppCompatActivity() {
 
         binding = ActivityGuardarSqliteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnGuardar2.setOnClickListener {
+            val nombre = binding.txtNombre.text.toString()
+            val apellido = binding.txtApellido.text.toString()
+            val email = binding.txtEmail.text.toString()
+            val celular = binding.txtCelular.text.toString()
+            val sucursal = binding.txtSucursal.text.toString()
+
+            lifecycleScope.launch {
+                val db = AppDatabase.getDatabase(this@GuardarSQLiteActivity)
+                val usuarioDAO = db.usuarioDao()
+
+                val usuario = Usuario(
+                    nombre = nombre,
+                    apellido = apellido,
+                    email = email,
+                    celular = celular,
+                    sucursal = sucursal
+                )
+                usuarioDAO.insert(usuario)
+
+            }
+            Toast.makeText(this, "Usuario guardado correctamente", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun guardarUsuarios() {
+
     }
 }
